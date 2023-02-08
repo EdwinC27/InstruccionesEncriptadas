8be17f2a2877010503c06362ffa39f293b98721d
Castillo Arroyo <kchv321@astrazeneca.net>
2023-02-08 16:51:03 -0600

    Sobrescribir el archivo que genero la respuesta si existe

--- src/GuardarArchivo.java
+++ src/GuardarArchivo.java
@@ -1,15 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 
 public class GuardarArchivo {
-    public static void crearArchivo(String file) {
-        try(FileWriter fw = new FileWriter(file, true))
-        {
-            fw.write(Atributos.resultado1);
-            fw.write("\n");
-            fw.write(Atributos.resultado2);
-            System.out.println("El fichero se ha creado correctamente");
+    public static void crearArchivo(String nombre) {
+        try {
+            File file = new File(nombre);
+            // Si el archivo no existe es creado
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(Atributos.resultado1);
+            bw.write("\n");
+            bw.write(Atributos.resultado2);
+            bw.close();
+            System.out.println("Archivo creado");
         } catch (Exception e) {
-            System.out.println("Error al agregar los datos");
+            System.out.println("Error al crear el archivo");
         }
     }
 }
